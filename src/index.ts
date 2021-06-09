db08d105d10ff92188b6052d387482b826a2cc14
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2021-06-09 03:34:52 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ require("dotenv").config();
 import { buildSchema } from "type-graphql";
 import { mongoose } from "@typegoose/typegoose";
 import { DATABASE_URL, JWT_SECRET, PORT } from "./constants";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, ExpressContext } from "apollo-server-express";
 import { customAuthChecker } from "./auth";
 import { GideContext } from "./models/context.model";
-import { ExpressContext } from "apollo-server-express";
 import jwt = require("jsonwebtoken");
 import cors = require("cors");
+import express = require("express");
 import { AuthModel } from "./models/auth.model";
+import { graphqlUploadExpress } from "graphql-upload";
 
 async function bootstrap() {
     // Coneccion con la base de datos
@@ -27,20 +28,17 @@ async function bootstrap() {
         authChecker: customAuthChecker
     });
 
+    const app = express();
+    app.use(cors({
+        credentials: true,
+        origin: "*"
+    }));
+
     const server = new ApolloServer({
         schema,
         introspection: true,
         playground: true,
-        uploads: {
-            maxFiles: 20,
-            maxFileSize: 1024 * 1024 * 50
-        },
-        cors: {
-            credentials: true,
-            origin: (origin, callback) => {
-                callback(null, true);
-            }
-        },
+        uploads: false,
         context: async function (expressContext: ExpressContext): Promise<GideContext> {
             const token = expressContext.req.headers.authorization?.split(" ");
 
@@ -71,9 +69,16 @@ async function bootstrap() {
         }
     });
 
-    server.listen(PORT);
+    app.use(graphqlUploadExpress({
+        maxFileSize: 10000000,
+        maxFiles: 10
+    }));
 
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    server.applyMiddleware({ app });
+
+    app.listen(PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    })
 }
 
 
